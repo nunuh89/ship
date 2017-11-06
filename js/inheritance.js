17b89f51cdb778e4718958bd87c09f6c9cf5357d
Chi Yang <nunuh89@gmail.com>
2017-11-06 14:54:06 -0800

    Completes inheritance

---
+++ js/inheritance.js
@@ -0,0 +1,22 @@
+// Function.prototype.inherits = function (parent) {
+//   function Surrogate() {}
+//   Surrogate.prototype = parent.prototype;
+//   this.prototype = new Surrogate();
+//   this.prototype.constructor = this;
+// };
+
+Function.prototype.inherits = function (parent) {
+  this.prototype = Object.create(parent.prototype);
+  this.prototype.constructor = this;
+};
+
+function MovingObject () {}
+MovingObject.prototype.move = function() {};
+
+
+function Ship () {}
+Ship.inherits(MovingObject);
+Ship.prototype.fly = function() {};
+
+function Asteroid () {}
+Asteroid.inherits(MovingObject);
